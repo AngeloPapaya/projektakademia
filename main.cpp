d974a004c050341707fc7030369a2fef9162c6de
AngeloPapaya <147622287+AngeloPapaya@users.noreply.github.com>
2024-01-29 19:19:00 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,38 @@
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+#include <windows.h>
+
+class SnakeGame
+{
+    void gotoxy (short x, short y)
+{
+    COORD pos = {x,y};
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),pos);
+}
+    void GoDown();
+    void GoUp();
+    void GoLeft();
+    void GoRight();
+};
+
+void gotoxy (short x, short y)
+{
+    COORD pos = {x,y};
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),pos);
+}
+
+int main()
+
+
+{
+
+char znak = getch();
+
+
+
+
+
+}
+
 
