b9bf19233293099df9363558cfef6264c16deb2c
AngeloPapaya <147622287+AngeloPapaya@users.noreply.github.com>
2024-02-14 08:14:45 +0100

    Update MyClass.h

--- MyClass.h
+++ MyClass.h
@@ -5,8 +5,26 @@ class SnakeGame
 
 public:
 
+    enum class Choice
+    {
+        Play = 0,
+        Options,
+        EndGame
+    };
+
+    enum class Keys
+    {
+        Up = 72,
+        Down = 80,
+        Right = 77,
+        Left = 75,
+        Keyboard = 224
+
+    };
+
     void gotoxy (short x, short y);
-    void Menu();
-    void KoniecGry();
+    void PrintMenu();
+    void PrintEndGameText();
+    void StartGame();
 
 };
