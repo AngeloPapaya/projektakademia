cea0845cc132047a4d4ca1afe8d0753ef1bdfc79
AngeloPapaya <147622287+AngeloPapaya@users.noreply.github.com>
2024-02-14 08:15:07 +0100

    Update MyClass.cpp

--- MyClass.cpp
+++ MyClass.cpp
@@ -10,7 +10,7 @@
         SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),pos);
     }
 
-    void SnakeGame::Menu()
+    void SnakeGame::PrintMenu()
     {
         HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
         SetConsoleTextAttribute(hConsole, 10);
@@ -35,7 +35,7 @@
         SetConsoleTextAttribute(hConsole, 4);
     }
 
-    void SnakeGame::KoniecGry()
+    void SnakeGame::PrintEndGameText()
     {
         system( "cls" );
         HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -43,5 +43,88 @@
         gotoxy(24,2);
         std::cout << "~~ Do zobaczenia nastepnym razem! ~~";
         SetConsoleTextAttribute(hConsole, 15);
+    }
+
+    void SnakeGame::StartGame()
+    {
+        SnakeGame snakeGame;
+        int wybor;
+        int poprzedniWybor;
+        bool MenuLoop;
+        bool MainLoop = true;
+
+        while(MainLoop) //glowna petla programu
+        {
+            wybor = 0;
+            poprzedniWybor = wybor;
+            MenuLoop = true;
+
+                    //rysowanie menu
+            system( "cls" );
+            snakeGame.PrintMenu();
+
+
+            while (MenuLoop)   //strzalki (gora, dol)
+            {
+                snakeGame.gotoxy( 9, wybor  + 4 );          //rysowanie strzalkani
+                std::cout << static_cast < char >( 16 );
+                poprzedniWybor = wybor;         //obslugga klawiatury
+                switch( getch() )
+                {
+                case 224:           // STRZALKI
+                    switch( static_cast<SnakeGame::Keys>(getch()) )
+                    {
+                    case SnakeGame::Keys::Up:              //strzalka w góre, ilosc opcji w menu (aktualnie dwie)
+                        if ( 0 < wybor )
+                        wybor--;
+                        else wybor = 2;
+                        break;
+
+                    case SnakeGame::Keys::Down:                    //strzalka w dól, ilosc opcji w menu (aktualnie dwie)
+                        if ( wybor < 2 )
+                        wybor++;
+                        else wybor = 0;
+                        break;
+
+                    case SnakeGame::Keys::Right: //strzalka w prawo //je¿eli wciœniêto strzalkê w prawo, sprawdz wybrana opcje i wykonaj odpowiednia akcje
+                    {
+
+
+                            Choice choice = static_cast<SnakeGame::Choice>(wybor);
+
+                            if ( choice == SnakeGame::Choice::Play )
+                            {
+                                 //tutaj bedzie kod snake'a
+                            }
+                            if ( choice == SnakeGame::Choice::Options ) //menu opcji
+                            {
+                                system( "cls" );
+                                break;
+                            }
+                            if ( choice == SnakeGame::Choice::EndGame )
+                            {
+                                system( "cls" );
+                                snakeGame.PrintEndGameText();
+                                MenuLoop = false;
+                                MainLoop = false;
+                                break;
+                            }
+                    }
+                    case SnakeGame::Keys::Left: //strzalka w lewo cofa do menu
+                        {
+                            wybor = 0;
+                            system( "cls" );
+                            snakeGame.PrintMenu();
+                            break;
+                        }
+                    }
+                snakeGame.gotoxy( 9, poprzedniWybor  + 4 ); //czyszczenie strzalki wyboru
+                std::cout << " ";
+
+                }
+            }
+
+
+        }
     }
 
